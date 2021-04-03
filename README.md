fcc980b8552f7904b4d2163e63e1d174ef3ef917
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-04-02 21:36:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Near-Earth-Objects
+ Allows users to search, investigate, and query NASA CSV and JSON files containing data on near Earth objects
---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+
+
+class Inspect:
+    pass
+
+
+class Query:
+    pass
+
+
+class Interactive:
+    pass
+
+
+f = open('cad.json', )
+data = json.load(f)
+print(data['fields'])
+for row in data['data']:
+    if row[0] == "2002 PB" and "2000-Jan-01" in row[3]:
+        for columns in range(11):
+            print(f"{data['fields'][columns]} {row[columns]}")
+f.close()
